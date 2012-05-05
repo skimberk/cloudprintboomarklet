498f1ae1be223361bf3cbf5ec6ea8286b2e66ba0
Sebastian Kimberk <skimberk1@gmail.com>
2012-05-05 19:09:57 -0400

    fixed stuff, broke stuff

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,18 +1,20 @@
-if($('#google_cloud_print_hg58hsr').length == 0){
-	$.getScript('//www.google.com/cloudprint/client/cpgadget.js', function(){
-		$('body').append('<div id="google_cloud_print_hg58hsr"></div>');
-		$('#google_cloud_print_hg58hsr').attr('style', 'position: fixed; top: 100px; left: 50%; margin-left: -50px; padding:20px 40px; text-align:center; vertical-align:middle; z-index: 999999; border:1px solid #fff; background:rgba(0,0,0,0.8); -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px;');
-		var addPrintButton = function() {
-			var gadget = new cloudprint.Gadget();
-			gadget.setPrintButton(cloudprint.Gadget.createDefaultPrintButton("google_cloud_print_hg58hsr"));
-			gadget.setPrintDocument("url", document.title, document.URL);
-		};
-
-		window.setTimeout(addPrintButton, 200);
-
-		$('#google_cloud_print_hg58hsr').click(function(){
-			$('#google_cloud_print_hg58hsr').remove();
-		});
-	});
-	console.log('Print dialog added.');
-}
+(function(){
+	var repeaty;
+	var init = function(){
+		jQuery.getScript('https://raw.github.com/skimberk1/cloudprintboomarklet/master/bookmarklet-run.js');
+	};
+	var s=document.createElement('script');
+	s.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js');
+	if(typeof jQuery == 'undefined'){
+		document.getElementsByTagName('head')[0].appendChild(s);
+		repeaty = setInterval(function(){
+			if(typeof jQuery != 'undefined'){
+				clearInterval(repeaty);
+				init();
+			}
+		}, 100);
+	}
+	else{
+		init();
+	}
+})();
